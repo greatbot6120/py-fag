a69f6636546a1e83b9ebc22b22deaf8615101423
greatbot6120 <liprandiivan@outlook.com>
2023-04-08 20:17:20 +0200

    picola correzione algoritmo per prime numnbers

--- lab3/es_lesson/es_multithread/tcp_prime_multithr_server.py
+++ lab3/es_lesson/es_multithread/tcp_prime_multithr_server.py
@@ -7,29 +7,28 @@ if __name__ == '__main__':
 
         while True:
 
+            flag_prime = True
             var_prime = connectionSocket.recv(2048)
             var_prime = var_prime.decode('utf-8')
+            var_prime = int(var_prime)
 
-            if (var_prime == 0) or (var_prime == 1):
+            if var_prime > 2:
 
-                response = "Il numero " + str(var_prime) + " NON E' un numero primo!"
+                for index in range(2, var_prime):
+
+                    if (var_prime % index) == 0:
+
+                        flag_prime = False
+                        break
+
+            if flag_prime:
+
+                response = "Il numero " + str(var_prime) + " E' un numero primo!"
                 connectionSocket.send(response.encode('utf-8'))
 
             else:
 
-                for index in range(2, var_prime):
-
-                    if not ((var_prime % index) == 0):
-
-                        flag_prime = True
-
-                    else:
-
-                        response = "Il numero " + str(var_prime) + " NON E' un numero primo!"
-                        connectionSocket.send(response.encode('utf-8'))
-                        break
-
-                response = "Il numero " + str(var_prime) + " E' un numero primo!"
+                response = "Il numero " + str(var_prime) + " NON E' un numero primo!"
                 connectionSocket.send(response.encode('utf-8'))
 
     serverPort = 60001
@@ -37,10 +36,11 @@ if __name__ == '__main__':
     tcpServerSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     tcpServerSocket.bind(('', serverPort))
     tcpServerSocket.listen(1)
+    print("Il server è pronto per ricevere!")
 
     while True:
 
-        print("Il server è pronto per ricevere!")
         varSocket, clientAddress = tcpServerSocket.accept()
+        print(f"Connessione da: {clientAddress}")
         threadPrimes = Thread(target=handler, args=(varSocket,))
         threadPrimes.start()
